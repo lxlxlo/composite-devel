cdf843b9add609ef02fa01884b8dae5eb9530e6e
Gabriel M. Beddingfield <gabriel@teuton.org>
2009-12-18 12:51:29 -0600

    Commit a bunch of tests for Tritium::Song (fails).

--- src/Tritium/test/t_Song.cpp
+++ src/Tritium/test/t_Song.cpp
@@ -20,17 +20,17 @@
  */
 
 /*
- * This test was originally to test the methods in the songhelpers.cpp
- * file.  However, these have now been merged into class Song.  Since
- * these tests are written and work, they are retained with the
- * t_songhelpers.cpp name.
+ * This test verifies the workings of Tritium::Song.
  */
 #include <Tritium/Engine.hpp>
 #include <Tritium/Song.hpp>
 #include <Tritium/Logger.hpp>
 #include <Tritium/Preferences.hpp>
+#include <Tritium/Pattern.hpp>
+#include <Tritium/Instrument.hpp>
+#include <Tritium/InstrumentList.hpp>
 
-#define THIS_NAMESPACE t_songhelpers
+#define THIS_NAMESPACE t_Song
 #include "test_macros.hpp"
 #include "test_config.hpp"
 
@@ -67,13 +67,132 @@ TEST_BEGIN( Fixture );
 
 TEST_CASE( 010_defaults )
 {
+    /*********************************
+     * Using constructor
+     *********************************
+     */
+    std::auto_ptr<Song> z( new Song("Empty", "Scripty", 134.9f, 0.8f ) );
+
+    // Metadata
+    CK( z->get_name() == "Empty" );
+    CK( z->get_volume() == 0.8f );
+    CK( z->get_metronome_volume() == 0.5f );
+    CK( z->get_mute() == false );
+    CK( z->get_resolution() == 48 );
+    CK( z->get_bpm() == 134.9f );
+    CK( z->get_modified() == false );
+    CK( z->get_author() == "Scripty" );
+    CK( z->get_license() == "" );
+    CK( z->get_filename() == "" );
+
+    // Pattern and Instrument
+    CK( z->get_pattern_list() == 0 );
+    CK( z->get_pattern_group_vector() == 0 );
+    CK( z->get_instrument_list() == 0 );
+    CK( z->get_notes() == "" );
+    CK( z->is_loop_enabled() == false );
+    CK( z->get_humanize_time_value() == 0.0 );
+    CK( z->get_humanize_velocity_value() == 0.0 );
+    CK( z->get_swing_factor() == 0 );
+    CK( z->get_mode() == Song::PATTERN_MODE );
+
+    // "songhelper" methods
+    CK( z->song_bar_count() == 0 ); // XXX This segfaults, and the rest prob do, too.
+    CK( z->song_tick_count() == 0 );
+    CK( z->pattern_group_index_for_bar(1) == -1 );
+    CK( z->bar_for_absolute_tick(0) == -1 );
+    CK( z->bar_start_tick(1) == -1 );
+    CK( z->ticks_in_bar(1) == -1 );
+
+    // Pattern Mode methods
+    CK( z->get_pattern_mode_type() == Song::SINGLE );
+
+    /*********************************
+     * Using static method
+     *********************************
+     */
+    std::auto_ptr<Song> w( Song::get_default_song(engine) );
+
+    // Metadata
+    CK( w->get_name() == "empty" );
+    CK( w->get_volume() == 1.0 );
+    CK( w->get_metronome_volume() == 0.5 );
+    CK( w->get_mute() == false );
+    CK( w->get_resolution() == 48 );
+    CK( w->get_bpm() == 120.0 );
+    CK( w->get_modified() == false );
+    CK( w->get_author() == "Tritium" );
+    CK( w->get_license() == "" );
+    CK( w->get_filename() == "" );
+
+    // Pattern and Instrument
+    CK( w->get_pattern_list() == 0 );
+    CK( w->get_pattern_group_vector() == 0 );
+    CK( w->get_instrument_list() == 0 );
+    CK( w->get_notes() == "..." );
+    CK( w->is_loop_enabled() == false );
+    CK( w->get_humanize_time_value() == 0.0 );
+    CK( w->get_humanize_velocity_value() == 0.0 );
+    CK( w->get_swing_factor() == 0 );
+    CK( w->get_mode() == Song::PATTERN_MODE );
+
+    // "songhelper" methods
+    CK( w->song_bar_count() == 0 );
+    CK( w->song_tick_count() == 0 );
+    CK( w->pattern_group_index_for_bar(1) == -1 );
+    CK( w->bar_for_absolute_tick(0) == -1 );
+    CK( w->bar_start_tick(1) == -1 );
+    CK( w->ticks_in_bar(1) == -1 );
+
+    // Pattern Mode methods
+    CK( w->get_pattern_mode_type() == Song::SINGLE );
+    Tritium::PatternList pl;
+    w->get_playing_patterns(pl);
+    CK( pl.get_size() == 0 );
+
+}
+
+TEST_CASE( 015_song_loading )
+{
     BOOST_MESSAGE( s->get_name().toStdString() );
-    CK( s->get_name() == QString("Jazzy") );
+
+    // Metadata
+    CK( s->get_name() == "Jazzy" );
+    CK( s->get_volume() == 0.5 );
+    CK( s->get_metronome_volume() == 0.5 );
+    CK( s->get_mute() == false );
+    CK( s->get_resolution() == 48 );
+    CK( s->get_bpm() == 100.0f );
+    CK( s->get_modified() == false );
+    CK( s->get_author() == "Emiliano Grilli" );
+    CK( s->get_license() == "" );
+    CK( s->get_filename() == "" );
+
+    // Pattern and Instrument
+    CK( s->get_pattern_list()->get_size() == 3 );
+    CK( s->get_pattern_group_vector()->size() == 8 );
+    CK( s->get_instrument_list()->get_size() == 32 );
+    CK( s->get_notes() == "Jazzy..." );
+    CK( s->is_loop_enabled() == true );
+    CK( s->get_humanize_time_value() == 0.0 );
+    CK( s->get_humanize_velocity_value() == 0.23f );
+    CK( s->get_swing_factor() == 0.44f );
+    CK( s->get_mode() == Song::SONG_MODE );
+
+    // "songhelper" methods
     CK( s->song_bar_count() == 8 );
     CK( s->song_tick_count() == 1536 );
-    CK( s->bar_for_absolute_tick( 0 ) == 1 );
-    CK( s->bar_start_tick( 1 ) == 0 );
-    CK( s->ticks_in_bar( 1 ) == 192 );
+    CK( s->pattern_group_index_for_bar(1) == 0 );
+    CK( s->bar_for_absolute_tick(0) == 1 );
+    CK( s->bar_start_tick(1) == 0 );
+    CK( s->ticks_in_bar(1) == 192 );
+
+    // Pattern Mode methods
+    CK( s->get_pattern_mode_type() == Song::SINGLE );
+    Tritium::PatternList pl;
+    s->get_playing_patterns(pl);
+    CK( pl.get_size() == 1 );
+
 }
 
 TEST_CASE( 020_pattern_group_index_for_bar )
