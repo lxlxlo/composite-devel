742329f7465f4fe6941baf3e889c806299ec5561
Gabriel M. Beddingfield <gabriel@teuton.org>
2010-03-14 09:01:32 -0500

    Refactor Presets and add iterator interface and tests.

--- src/Tritium/Tritium/Presets.hpp
+++ src/Tritium/Tritium/Presets.hpp
@@ -32,14 +32,26 @@ namespace Tritium
 
     /**
      * A bank of presets (programs)
+     *
+     * You will typically /not/ access this class directly unless you
+     * are querying using the iterators.  You will usually access the
+     * presets through Tritium::Presets object.
      */
     class Bank
     {
-	typedef std::map<uint8_t, QString> map_t;
     public:
+	typedef std::map<uint8_t, QString> map_t;
+	typedef map_t::value_type pair_t;
+	typedef map_t::value_type value_type;
+	typedef map_t::iterator iterator;
+	typedef map_t::const_iterator const_iterator;
+
 	Bank() {}
 	~Bank() {}
 
+	/**
+	 * Retrieve the preset URI for a channel.
+	 */
 	const QString& program(uint8_t num) const {
 	    map_t::const_iterator it;
 	    it = _programs.find(num);
@@ -49,11 +61,29 @@ namespace Tritium
 	    return _none;
 	}
 
+	/**
+	 * Assigns a URI to a channel.
+	 *
+	 * To remove a program, use uri = ""
+	 */
 	void program(uint8_t num, const QString& uri) {
 	    if(num < 128) {
-		_programs[num] = uri;
+		if(uri != "") {
+		    _programs[num] = uri;
+		} else {
+		    iterator it = _programs.find(num);
+		    if( it != _programs.end() ) {
+			_programs.erase(it);
+		    }
+		}
 	    }
 	}
+
+	// ITERATOR ACCESS
+	iterator begin() { return _programs.begin(); }
+	const_iterator begin() const { return _programs.begin(); }
+	iterator end() { return _programs.end(); }
+	const_iterator end() const { return _programs.end(); }
 
     private:
 	map_t _programs;
@@ -61,45 +91,99 @@ namespace Tritium
     }; // class Bank
 
     /**
-     * Class that manages a set of preset banks.
+     * Class that manages user presets.
+     *
+     * In simple terms, this class manages a bunch of Tritium::Bank
+     * objects.  However, you usually do not need to access the Bank
+     * objects directly.  The only reason to do so is if you're
+     * querying the presets with the iterator access.
      */
     class Presets
     {
-	typedef std::map<uint16_t, Bank> map_t;
-
-	inline static uint16_t _8_16(uint8_t coarse, uint8_t fine) {
-	    uint16_t rv = ((coarse & 0x7F) << 7) | (fine & 0x7F);
-	    return rv;
-	}
-
     public:
+	struct bank_address_t {
+	    uint8_t coarse;
+	    uint8_t fine;
+
+	    int operator<(const bank_address_t& right) const {
+		if(coarse == right.coarse) {
+		    return fine < right.fine;
+		}
+		return coarse < right.coarse;
+	    }
+	};
+
+	typedef std::map<bank_address_t, Bank> map_t;
+	typedef map_t::value_type pair_t;
+	typedef map_t::value_type value_type;
+	typedef map_t::iterator iterator;
+	typedef map_t::const_iterator const_iterator;
+
 	Presets() {}
 	~Presets() {}
 
-	const QString& program(uint8_t coarse, uint8_t fine, uint8_t prog) const {
-	    uint16_t bank = _8_16(coarse, fine);
-	    return program(bank, prog);
+    protected:
+	inline static bool valid(uint8_t coarse, uint8_t fine, uint8_t prog) {
+	    return (coarse < 128) && (fine < 128) && (prog < 128);
 	}
 
-	const QString& program(uint16_t bank, uint8_t prog) const {
-	    map_t::const_iterator it;
-	    it = _banks.find(bank);
-	    if(it != _banks.end()) {
-		return it->second.program(prog);
+    public:
+	/**
+	 * Returns the URI stored at bank (coarse, fine) and program number.
+	 *
+	 * If none is stored, an empty string will be returned.
+	 */
+	const QString& program(uint8_t coarse, uint8_t fine, uint8_t prog) const {
+	    if( valid(coarse, fine, prog) ) {
+		bank_address_t bank = {coarse, fine};
+		map_t::const_iterator it;
+		it = _banks.find(bank);
+		if(it != _banks.end()) {
+		    return it->second.program(prog);
+		}
 	    }
 	    return _none;
 	}
 
-	void set_program(uint8_t coarse, uint8_t fine, uint8_t prog, QString uri) {
-	    uint16_t bank = _8_16(coarse, fine);
-	    set_program(bank, prog, uri);
+	/**
+	 * Overloaded method for convenience.
+	 */
+	const QString& program(uint16_t bank, uint8_t prog) const {
+	    if( bank == (bank & 0x3FFF) ) {
+		uint8_t fine = bank & 0x7F;
+		uint8_t coarse = bank >> 7; // allowing invalid
+		return program(coarse, fine, prog);
+	    }
+	    return _none;
 	}
 
-	void set_program(uint16_t bank, uint8_t prog, QString uri) {
-	    if(_banks.find(bank) == _banks.end()) {
-		_banks[bank] = Bank();
+	/**
+	 * Assign a URI to a specific bank (coarse, fine) and program number.
+	 *
+	 * To delete the preset, simply set uri = "".
+	 */
+	void set_program(uint8_t coarse, uint8_t fine, uint8_t prog, QString uri) {
+	    /* Note that when URI == "", the Bank itself is /not/ being
+	     * deleted.  No real reason... just doesn't seem necc.
+	     */
+	    if( valid(coarse, fine, prog) ) {
+		bank_address_t bank = {coarse, fine};
+		if(_banks.find(bank) == _banks.end()) {
+		    _banks[bank] = Bank();
+		}
+		_banks[bank].program(prog, uri);
 	    }
-	    _banks[bank].program(prog, uri);
+	}
+
+	/**
+	 * Overloaded method for convenience.
+	 */
+	void set_program(uint16_t bank, uint8_t prog, QString uri) {
+	    if( bank == (bank & 0x3FFF) ) {
+		uint8_t fine = bank & 0x7F;
+		uint8_t coarse = bank >> 7; // allowing invalid
+		set_program(coarse, fine, prog, uri);
+	    }
 	}
 
 	void clear() {
@@ -107,6 +191,12 @@ namespace Tritium
 	}
 
 	void generate_default_presets(T<Preferences>::shared_ptr prefs);
+
+	// ITERATOR ACCESS
+	iterator begin() { return _banks.begin(); }
+	const_iterator begin() const { return _banks.begin(); }
+	iterator end() { return _banks.end(); }
+	const_iterator end() const { return _banks.end(); }
 
     private:
 	map_t _banks;
