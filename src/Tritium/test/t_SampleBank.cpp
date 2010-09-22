339f7be86f996c0bd6cf3e014adf1fdf07b0bb40
Gabriel M. Beddingfield <gabriel@teuton.org>
2010-09-22 12:47:20 -0500

    t_SampleBank: Add more tests to check values.

--- src/Tritium/test/t_SampleBank.cpp
+++ src/Tritium/test/t_SampleBank.cpp
@@ -26,6 +26,7 @@
 
 #include <Tritium/SampleBank.hpp>
 #include <deque>
+#include <algorithm>
 
 // CHANGE THIS TO MATCH YOUR FILE:
 #define THIS_NAMESPACE t_TestTemplate
@@ -102,6 +103,70 @@ TEST_CASE( 020_push_pop )
 	a.pop( *ki );
 	CK( a.size() == N );
     }
+}
+
+TEST_CASE( 020_value_integrity )
+{
+    unsigned vals[] = { 0x7a7b42c3, 0x0ef40b83, 0x1d8e9844, 0x42cf363a,
+			0x68c5a0aa, 0x4bb233c6, 0x0ae29622, 0x044ef6f6,
+			0x41ab4d45, 0x6c08187f, 0x67a9f64d, 0x65a7b89b,
+			0x0c104bb0, 0x23f35304, 0x3ed9fc94, 0x3665d570,
+			0xb92cbe2d, 0xf7da0f7e, 0x40c7ced4, 0x2f455865,
+			0x1ea63252, 0x46bff81c, 0x646e40ca, 0xd3995c53,
+			0x0f72bdbb, 0xe4703cdf, 0xc4ed168e, 0xb1584216,
+			0xff03505c, 0x48cbd37d, 0xae7eabfe, 0xbea37bc8,
+			0x0 };
+
+    // Create a bunch of samples
+    typedef std::deque< T<Sample>::shared_ptr > sample_array_t;
+    sample_array_t sample_array;
+
+    unsigned *vi;
+    for( vi = vals ; *vi ; ++vi ) {
+	T<Sample>::shared_ptr tmp( new Sample );
+	tmp->val = *vi;
+	sample_array.push_back(tmp);
+    }
+    CK( sample_array.size() == ((sizeof(vals)-1)/sizeof(unsigned)) );
+
+    // Add samples to the bank and track the keys
+    typedef std::deque< SampleBank::key_t > key_array_t;
+    key_array_t key_array;
+
+    sample_array_t::iterator sit;
+    for( sit = sample_array.begin() ; sit != sample_array.end() ; ++sit ) {
+	SampleBank::key_t key;
+	key = a.push( *sit );
+	CK( key != 0 );
+	CK( std::find(key_array.begin(), key_array.end(), key) == key_array.end() );
+	key_array.push_back( key );
+	CK( (*sit)->val == a.get(key)->val );
+    }
+
+    CK( key_array.size() == sample_array.size() );
+    CK( a.size() == sample_array.size() );
+
+    for( sit = sample_array.begin() ; sit != sample_array.end() ; ++sit ) {
+	CK( sit->use_count() == 2 );
+    }
+
+    // Check values back and forth
+    key_array_t::iterator kit;
+    for( sit = sample_array.begin(), kit = key_array.begin() ;
+	 sit != sample_array.end() ;
+	 ++sit, ++kit ) {
+	CK( kit != key_array.end() );
+
+	CK( (*kit) == a.find_key(*sit) );
+	CK( (*sit) == a.get(*kit) );
+    }
+
+    a.clear();
+    CK( a.size() == 0 );
+    for( sit = sample_array.begin() ; sit != sample_array.end() ; ++sit ) {
+	CK( sit->use_count() == 1 );
+    }
+   
 }
 
 TEST_END()
