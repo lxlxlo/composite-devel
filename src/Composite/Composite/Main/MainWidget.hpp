0874ecc4986631a657dd28f5e141fde31aef9398
Gabriel M. Beddingfield <gabriel@teuton.org>
2010-09-23 07:39:24 -0500

    Composite::Main::MainWidget: Create the MainWidget.
    MainWindow isn't quite what I thought it was.  Whether or not we use
    the ToolBars with MainWindow... we still need MainWidget to be the
    central widget.

--- src/Composite/Composite/Main/MainWidget.hpp
+++ src/Composite/Composite/Main/MainWidget.hpp
@@ -18,36 +18,75 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-#ifndef COMPOSITE_MAIN_MAINWINDOW_HPP
-#define COMPOSITE_MAIN_MAINWINDOW_HPP
+#ifndef COMPOSITE_MAIN_MAINWIDGET_HPP
+#define COMPOSITE_MAIN_MAINWIDGET_HPP
 
-#include <QtGui/QMainWindow>
+#include <QtGui/QWidget>
 #include <Tritium/memory.hpp>
 
 class QAction;
-class QToolBar;
 class QToolButton;
+class QToolBar;
 
 namespace Composite
 {
 namespace Main
 {
-
-    class MainWindow : public QMainWindow
+    /**
+     * \brief The central workspace for Composite
+     */
+    class MainWidget : public QWidget
     {
 	Q_OBJECT
     public:
-	MainWindow(QWidget *parent = 0);
-	virtual ~MainWindow();
+	MainWidget(QWidget *parent = 0);
+	virtual ~MainWidget();
+
+    public slots:
+	void go_matrix();
+	void go_edit();
+
+	void x_play();
+	void x_stop();
 
     private:
 	void _setup_actions();
 	void _setup_widgets();
 	void _layout_widgets();
 	void _setup_signals_and_slots();
+
+    private:
+	struct actions_t {
+	    // Go to...
+	    QAction *go_matrix;
+	    QAction *go_edit;
+
+	    // Transport
+	    QAction *x_play;
+	    QAction *x_stop;
+
+	    QAction *_end; // should be null
+	} _act;
+
+	struct tool_buttons_t {
+	    // Go to...
+	    QToolButton *go_matrix;
+	    QToolButton *go_edit;
+
+	    // Transport
+	    QToolButton *x_play;
+	    QToolButton *x_stop;
+
+	    QToolButton *_end; // should be null
+	} _tbtn;
+
+	struct tool_bar_t {
+	    QToolBar *main;
+	} _tbar;
+
     };
 
 } // namespace Main
 } // namespace Composite
 
-#endif // COMPOSITE_MAIN_MAINWINDOW_HPP
+#endif // COMPOSITE_MAIN_MAINWIDGET_HPP
---
+++ src/Composite/src/Main/MainWidget.cpp
@@ -0,0 +1,168 @@
+/*
+ * Copyright(c) 2010 by Gabriel M. Beddingfield <gabriel@teuton.org>
+ *
+ * This file is part of Composite
+ *
+ * Composite is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Composite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <Composite/Main/MainWidget.hpp>
+
+#include <QtGui/QAction>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QMessageBox>
+#include <QtGui/QToolBar>
+#include <QtGui/QToolButton>
+
+namespace Composite
+{
+namespace Main
+{
+
+    MainWidget::MainWidget(QWidget *parent) :
+	QWidget(parent)
+    {
+	_setup_actions();
+	_setup_widgets();
+	_layout_widgets();
+	_setup_signals_and_slots();
+	show();
+    }
+
+    MainWidget::~MainWidget()
+    {
+    }
+
+    /**
+     * \brief Go to the matrix view
+     */
+    void MainWidget::go_matrix()
+    {
+	QMessageBox::information(this, "go_matrix()", "go_matrix()");
+    }
+
+    /**
+     * \brief Go to the edit view
+     */
+    void MainWidget::go_edit()
+    {
+	QMessageBox::information(this, "go_edit()", "go_edit()");
+    }
+
+    /**
+     * \brief Transport: start playing
+     */
+    void MainWidget::x_play()
+    {
+	QMessageBox::information(this, "x_play()", "x_play()");
+    }
+
+    /**
+     * \brief Transport: stop playing
+     */
+    void MainWidget::x_stop()
+    {
+	QMessageBox::information(this, "x_stop()", "x_stop()");
+    }
+
+    /**
+     * \brief Create and connect generic actions
+     */
+    void MainWidget::_setup_actions()
+    {
+	_act.go_matrix = new QAction( "M", this );
+	_act.go_matrix->setToolTip( tr("Go to matrix view") );
+	addAction( _act.go_matrix );
+	connect( _act.go_matrix, SIGNAL(triggered()),
+		 this, SLOT(go_matrix()) );
+
+	_act.go_edit = new QAction( "E", this );
+	_act.go_edit->setToolTip( tr("Go to edit view") );
+	addAction( _act.go_edit );
+	connect( _act.go_edit, SIGNAL(triggered()),
+		 this, SLOT(go_edit()) );
+
+	_act.x_play = new QAction( "P", this );
+	_act.x_play->setToolTip( tr("Start playing") );
+	addAction( _act.x_play );
+	connect( _act.x_play, SIGNAL(triggered()),
+		 this, SLOT(x_play()) );
+
+	_act.x_stop = new QAction( "S", this );
+	_act.x_stop->setToolTip( tr("Stop playing") );
+	addAction( _act.x_stop );
+	connect( _act.x_stop, SIGNAL(triggered()),
+		 this, SLOT(x_stop()) );
+
+	_act._end = 0;
+    }
+
+    void MainWidget::_setup_widgets()
+    {
+#if 1
+	_tbtn.go_matrix = new QToolButton(this);
+	_tbtn.go_matrix->setDefaultAction(_act.go_matrix);
+
+	_tbtn.go_edit = new QToolButton(this);
+	_tbtn.go_edit->setDefaultAction(_act.go_edit);
+
+	_tbtn.x_play = new QToolButton(this);
+	_tbtn.x_play->setDefaultAction(_act.x_play);
+
+	_tbtn.x_stop = new QToolButton(this);
+	_tbtn.x_stop->setDefaultAction(_act.x_stop);
+#else
+	_tbtn.go_matrix = 0;
+	_tbtn.go_edit = 0;
+	_tbtn.x_play = 0;
+	_tbtn.x_stop = 0;
+#endif
+	_tbtn._end = 0;
+
+    }
+
+    void MainWidget::_layout_widgets()
+    {
+#if 1
+	QHBoxLayout *lay = new QHBoxLayout;
+
+	lay->addWidget(_tbtn.go_matrix);
+	lay->addWidget(_tbtn.go_edit);
+	lay->addStretch();
+	lay->addWidget(_tbtn.x_play);
+	lay->addWidget(_tbtn.x_stop);
+
+	setLayout(lay);
+#else
+	_tbar.main = new QToolBar(this);
+
+	_tbar.main->addAction( _act.go_matrix );
+	_tbar.main->addAction( _act.go_edit );
+	_tbar.main->addSeparator();
+	_tbar.main->addAction( _act.x_play );
+	_tbar.main->addAction( _act.x_stop );
+
+	addToolBar(_tbar.main);
+#endif
+    }
+
+    void MainWidget::_setup_signals_and_slots()
+    {
+    }
+
+} // namespace Main
+} // namespace Composite
+
