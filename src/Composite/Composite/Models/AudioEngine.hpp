6db767c47987051adeb113b093ba7e672f67d156
Gabriel M. Beddingfield <gabriel@teuton.org>
2010-09-30 21:33:13 -0500

    Composite: Start a model for the audio engine.

---
+++ src/Composite/Composite/Models/AudioEngine.hpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright(c) 2010 by Gabriel M. Beddingfield <gabriel@teuton.org>
+ *
+ * This file is part of Composite
+ *
+ * Composite is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Composite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+#ifndef COMPOSITE_MODELS_AUDIOENGINE_HPP
+#define COMPOSITE_MODELS_AUDIOENGINE_HPP
+
+#include <QtCore/QAbstractItemModel>
+#include <QtCore/QVariant>
+#include <Tritium/memory.hpp>
+
+class QPaintEvent;
+
+namespace Composite
+{
+namespace Models
+{
+    class AudioEnginePrivate;
+
+    /**
+     * \brief The main access to the Tritium Engine.
+     */
+    class AudioEngine : public QAbstractItemModel
+    {
+	Q_OBJECT
+    private:
+	AudioEnginePrivate * const _d;
+
+    public:
+	AudioEngine(int argc = 0, char* argv[] = 0, QObject *parent = 0);
+	virtual ~AudioEngine();
+
+	// Reimplementing QAbstractItemModel methods
+	virtual int columnCount( const QModelIndex & parent = QModelIndex() ) const;
+	virtual int rowCount( const QModelIndex & parent = QModelIndex() ) const;
+
+	virtual QVariant data( const QModelIndex & index,
+			       int role = Qt::DisplayRole ) const;
+	virtual QModelIndex index( int row,
+				    int column,
+				    const QModelIndex & parent = QModelIndex() ) const ;
+	virtual QModelIndex parent( const QModelIndex & index ) const;
+    };
+
+} // namespace Models
+} // namespace Composite
+
+#endif // COMPOSITE_MODELS_AUDIOENGINE_HPP
---
+++ src/Composite/src/Models/AudioEngine.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright(c) 2010 by Gabriel M. Beddingfield <gabriel@teuton.org>
+ *
+ * This file is part of Composite
+ *
+ * Composite is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Composite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <Composite/Models/AudioEngine.hpp>
+
+#include "AudioEnginePrivate.hpp"
+
+namespace Composite
+{
+namespace Models
+{
+
+    AudioEngine::AudioEngine(int argc, char* argv[], QObject *parent) :
+	QAbstractItemModel(parent),
+	_d( new AudioEnginePrivate(this) )
+    {
+    }
+
+    AudioEngine::~AudioEngine()
+    {
+    }
+
+    int AudioEngine::columnCount( const QModelIndex & parent ) const
+    {
+	return 4;
+    }
+
+    int AudioEngine::rowCount( const QModelIndex & parent ) const
+    {
+	return 6;
+    }
+
+    QVariant AudioEngine::data( const QModelIndex & index,
+			       int role ) const
+    {
+	return QVariant();
+    }
+
+    QModelIndex AudioEngine::index( int row,
+				    int column,
+				    const QModelIndex & parent ) const
+    {
+	return QModelIndex();
+    }
+
+    QModelIndex AudioEngine::parent( const QModelIndex & index ) const
+    {
+	return QModelIndex();
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    // AudioEnginePrivate
+    ////////////////////////////////////////////////////////////////////
+
+    AudioEnginePrivate::AudioEnginePrivate(AudioEngine *p) :
+	_parent(p)
+    {
+    }
+
+    AudioEnginePrivate::~AudioEnginePrivate()
+    {
+    }
+
+
+} // namespace Models
+} // namespace Composite
+
